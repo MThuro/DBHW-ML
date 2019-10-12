e08511c32fb403eca8637a0d0f977a3421dfa5f9
MThuro <mareike.thurau@t-online.de>
2019-10-12 14:08:45 +0200

    Standardize input and vary neurons

--- NN classification.py
+++ NN classification.py
@@ -12,16 +12,23 @@ import time
 from tensorflow import keras
 from tensorflow import feature_column
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 from random import random
 
 tf.enable_eager_execution() 
+
+# Add preprocessor
+scaler = preprocessing.StandardScaler()
 
 # Read data from csv file
 dataframe = pd.read_csv("../data_u_1_3001879_5279494.csv",sep=';')
 # Convert column "Einordnung" into a discrete numerical value
 dataframe['Einordnung'] = pd.Categorical(dataframe['Einordnung'])
 dataframe['Einordnung'] = dataframe.Einordnung.cat.codes
+
+# Standardize numeric columns in order to increase accuracy
+dataframe[['Groesse', 'Gewicht', 'Alter']] = scaler.fit_transform(dataframe[['Groesse', 'Gewicht', 'Alter']])
 
 # Split data into train, test and validation examples
 train, test = train_test_split(dataframe, test_size=0.2)
@@ -37,22 +44,23 @@ def df_to_dataset(dataframe, shuffle=True, batch_size=78):
   ds = ds.batch(batch_size)
   return ds
 
+# Create feature layer
 def create_feature_layer() -> keras.layers.DenseFeatures:
-  # feature column for height
+  # Feature column for height
   feature_height = feature_column.numeric_column("Groesse")
 
-  # feature column for weight
+  # Feature column for weight
   feature_weight = feature_column.numeric_column("Gewicht")
 
-  # feature column for age
+  # Feature column for age
   feature_age = feature_column.numeric_column("Alter") 
 
-  # category column for gender
+  # Category column for gender
   feature_gender = feature_column.categorical_column_with_vocabulary_list(
         'Geschlecht', ['w', 'm'])
   feature_gender_one_hot = feature_column.indicator_column(feature_gender)
 
-  # category column for activities  
+  # Category column for activities  
   feature_activities = feature_column.categorical_column_with_vocabulary_list(
          'Betaetigung', ['keinSport', 'Kraftsport', 'Ausdauersport'])
   feature_activities_one_hot = feature_column.indicator_column(feature_activities)
@@ -62,9 +70,9 @@ def create_feature_layer() -> keras.layers.DenseFeatures:
 
   return tf.keras.layers.DenseFeatures(feature_columns)
 
-# define model
+# Define model
 def define_model(neuron_layer_1: int = 128, neuron_layer_2: int = 128) -> tf.keras.Sequential:
-  #get feature layer
+  # Get feature layer
   feature_layer = create_feature_layer()
 
   model = tf.keras.Sequential([
@@ -80,41 +88,55 @@ def define_model(neuron_layer_1: int = 128, neuron_layer_2: int = 128) -> tf.ker
                 run_eagerly=True)
   return model                           
 
-# train model
+# Train model
 def train_model():
-      # build data set
+      best_test_acc = 0
+      best_model = None
+
+      # Build data set
       train_ds = df_to_dataset(train)
       val_ds = df_to_dataset(val, shuffle=False)
       test_ds = df_to_dataset(test, shuffle=False)
 
-      # get model
-      model = define_model(111, 113)
-      model.fit(train_ds,
-                validation_data=val_ds,
-                epochs=5)
+      # Get model
+      for x in range (0,3):
+        neuron_layer_1 = np.random.randint(50, 150)
+        neuron_layer_2 = np.random.randint(50, 150)
 
-      # evaluate model on test data to see accuracy
-      test_loss, test_accuracy = model.evaluate(test_ds, verbose=0)
-      # evaluate model on train data to crosscheck with tets data evaluation --> avoid overfitting
-      train_loss, train_accuracy = model.evaluate(train_ds, verbose=0)
-      return test_accuracy, model
+        model = define_model(neuron_layer_1, neuron_layer_2)
+        model.fit(train_ds,
+                  validation_data=val_ds,
+                  epochs=5)
 
-#predict classification for test data
+        # Evaluate model on test data to see accuracy
+        test_loss, test_accuracy = model.evaluate(test_ds, verbose=0)
+
+        # Compare accucary -> save best model
+        if test_accuracy > best_test_acc:
+          best_test_acc = test_accuracy
+          best_model = model
+      return best_test_acc, best_model
+
+# Predict classification for test data
 def predict(model: tf.keras.Sequential):
   test_ds = df_to_dataset(dataframe, shuffle=False)
   predictions = model.predict(test_ds)    
 
-  # print results
+  # Process results
   result = pd.DataFrame(predictions, columns=["Normalgewicht", "Übergewicht", "Untergewicht"])
   result['Normalgewicht'] = pd.Series(["{0:.2f}%".format(val * 100) for val in result['Normalgewicht']], index = result.index)
   result['Übergewicht'] = pd.Series(["{0:.2f}%".format(val * 100) for val in result['Übergewicht']], index = result.index)
   result['Untergewicht'] = pd.Series(["{0:.2f}%".format(val * 100) for val in result['Untergewicht']], index = result.index)
 
-  # save to csv
+  # Save to csv
   file_name = "./predictions" + time.strftime("%d%m%Y%H%M%S") + ".csv"
   result.to_csv(file_name, sep=';', encoding='utf-8')     
   print("File has been saved under", file_name)
       
-#execute training
+# Execute training
 test_accuracy, model = train_model()
+print(test_accuracy)
+
+# Predict categories based on learned model
 predict(model)
+
